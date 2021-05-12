ec6df2796a226f17f1f8f749eb4932fcd0be854a
Marijana Katalinić <marakata865@gmail.com>
2021-05-12 17:50:08 +0200

    prvi

---
+++ firmajp24.sql
@@ -0,0 +1,18 @@
+drop database if exists zupanija;
+create database zupanija;
+use zupanija;
+
+create table zupanija(
+sifra int not null primary key auto_increment,
+naziv varchar (50) not null,
+zupan int not null
+);
+
+create table opcina( 
+sifra int not null primary key auto_increment,
+naziv varchar(50) not null,
+zupanija int not null
+);
+
+alter table opcina add foreign key (zupanija) references zupanija (sifra)
+alter table zupanija add foreign key (opcina) references opcina (sifra)
